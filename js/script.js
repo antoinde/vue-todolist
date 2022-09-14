806cdf784501880d4971cc4e8e2ac9a67b222461
antoinde <antonello.indelicato98@gmail.com>
2022-09-14 18:39:26 +0200

    Milestone 1 completato

--- js/script.js
+++ js/script.js
@@ -22,10 +22,45 @@ console.log("JavaScript is working properly! =) Let's code!");
     Predisporre un campo di input testuale e un pulsante "aggiungi": 
     cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare 
     un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-    
+
     Bonus:
     1- oltre al click sul pulsante, intercettare anche il tasto ENTER per 
     aggiungere il todo alla lista
     2- cliccando sul testo dell'item, invertire il valore della proprietà done del 
     todo corrispondente (se done era uguale a false, impostare true e viceversa)
 */
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos:[
+            {
+                text: 'Dare cibo al cane',
+                done: false
+            },
+            {
+                text: 'Portare a spasso il cane',
+                done: false
+            },
+            {
+                text: 'Studiare VueJS',
+                done: true
+            },
+            {
+                text: 'Andare in palestra',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                text: 'Fornire amore alla fidanzata',
+                done: false
+            }
+            
+        ]
+    },
+    
+});
+
